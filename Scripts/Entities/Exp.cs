3825355a8e9939d1b733e175ebc54988b17c62e2
Radek Novotny <radek.novotny@autodesk.com>
2025-12-11 16:31:08 +0100

    WIP

--- Scripts/Entities/Exp.cs
+++ Scripts/Entities/Exp.cs
@@ -1,9 +1,18 @@
 using Godot;
+using Godot.Collections;
 using System;
 
 public partial class Exp : Area2D, ICollectible
 {
+    [Export] Array<Texture2D> textures = new();
+    [Export] Sprite2D sprite;
     private int xpAmount = 10;
+
+    public override void _Ready()
+    {
+        Random random = new Random();
+        sprite.Texture = textures[(int)random.NextInt64(textures.Count)];
+    }
 
     public void Init(int xpAmount)
     {
